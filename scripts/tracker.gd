363071eb5eb817a5283fc0a824d3200ed6a84433
Artin The Coder <artin.fallah2010@gmail.com>
2025-04-05 10:21:34 -0400

    made it possible to save & load the title of the list

--- scripts/tracker.gd
+++ scripts/tracker.gd
@@ -1,7 +1,11 @@
 extends Control
+
 var task_scene = preload("res://scenes/task.tscn")
+
 @onready var active_tasks_container = $ScrollContainer/VBoxContainer/Tasks/VBoxContainer
 @onready var finished_tasks_container = $ScrollContainer/VBoxContainer/FinishedTasks/VBoxContainer
+@onready var agenda_title: LineEdit = $Title/TitleText
+
 const SAVE_PATH = "user://tasks.json"
 
 func _ready():
@@ -47,15 +51,24 @@ func delete_task(task):
 	save_tasks()
 	
 func save_tasks():
-	var tasks_data = []
+	# Create a data structure that includes both tasks and the title
+	var save_data = {
+		"agenda_title": agenda_title.text,  # Save the title text
+		"tasks": []  # Array to hold task data
+	}
+	
+	# Collect all tasks as before
 	for task in active_tasks_container.get_children() + finished_tasks_container.get_children():
 		if task.has_method("get_task_data"):
 			var task_data = task.get_task_data()
 			task_data["task_moved"] = task.get_parent() == finished_tasks_container
-			tasks_data.append(task_data)
+			save_data["tasks"].append(task_data)
+	
+	# Save everything to the JSON file
 	var file = FileAccess.open(SAVE_PATH, FileAccess.WRITE)
-	file.store_string(JSON.stringify(tasks_data))
-	print("Tasks saved!")
+	file.store_string(JSON.stringify(save_data))
+	print("Tasks and title saved!")
+	
 	
 func load_tasks():
 	if not FileAccess.file_exists(SAVE_PATH):
@@ -68,17 +81,28 @@ func load_tasks():
 		print("JSON Parse Error: ", json.get_error_message())
 		return
 	
+	var save_data = json.get_data()
+	
+	# Load the agenda title if it exists in the saved data
+	if save_data.has("agenda_title"):
+		agenda_title.text = save_data["agenda_title"]
+	
+	# Clear existing tasks
 	for child in active_tasks_container.get_children() + finished_tasks_container.get_children():
 		child.queue_free()
-	for task_data in json.get_data():
-		var new_task = task_scene.instantiate()
-		var container = finished_tasks_container if task_data.get("task_moved", false) else active_tasks_container
-		container.add_child(new_task)
-		new_task.task_completed.connect(move_task_to_finished)
-		new_task.task_edited.connect(save_tasks)
-		new_task.task_deleted.connect(delete_task)  # Connect the deleted signal for loaded tasks
-		new_task.set_task_data(task_data)
-	print("Tasks loaded!")
+	
+	# Load tasks if they exist
+	if save_data.has("tasks"):
+		for task_data in save_data["tasks"]:
+			var new_task = task_scene.instantiate()
+			var container = finished_tasks_container if task_data.get("task_moved", false) else active_tasks_container
+			container.add_child(new_task)
+			new_task.task_completed.connect(move_task_to_finished)
+			new_task.task_edited.connect(save_tasks)
+			new_task.task_deleted.connect(delete_task)
+			new_task.set_task_data(task_data)
+	
+	print("Tasks and title loaded!")
 	
 func _on_save_button_pressed():
 	save_tasks()
